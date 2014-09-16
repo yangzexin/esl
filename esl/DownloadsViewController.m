a87d6b5333de67109c45252b81983eec6b0d96b2
yangzexin-imac <yangzexin2011@gmail.com>
2014-09-16 11:23:36 +0800

    c

--- esl/DownloadsViewController.m
+++ esl/DownloadsViewController.m
@@ -41,7 +41,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self setLeftBarButtonItemAsSideMenuSwitcher];
     
     @weakify(self);
     [self addRepositionSupportedObject:[SFRepeatTimer timerStartWithTimeInterval:1.0f tick:^{
